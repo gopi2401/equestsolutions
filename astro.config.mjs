244ad614afd9015cae42f9baf97a41b04db249f9
gopi2401 <gobinath@streamflo.io>
2025-02-02 12:47:14 +0530

    fix: base issue

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,14 @@
-import { defineConfig } from 'astro/config';
-import react from '@astrojs/react';
+import { defineConfig } from "astro/config";
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-    // output: 'static',
-    // site: 'https://gopi2401.github.io/portfolio/equestsolutions/',
-    base: '/portfolio/equestsolutions/',
-    // trailingSlash: "always",
-    build: {
-        assets: 'assets'
-    },
-    integrations: [react({ experimentalReactChildren: true })]
+  // output: 'static',
+  site: "https://gopi2401.github.io/",
+  base: "/equestsolutions/",
+  // trailingSlash: "always",
+  build: {
+    assets: "assets",
+  },
+  integrations: [react({ experimentalReactChildren: true })],
 });
